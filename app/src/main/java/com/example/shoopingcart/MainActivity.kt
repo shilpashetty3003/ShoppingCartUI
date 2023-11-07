6c7dea7f84b29987dc73e02b29b9e5ef37474c4f
Shilpa <shilpashetty3003@gmail.com>
2023-11-07 16:53:21 +0530

    MainActivity File

--- app/src/main/java/com/example/shoopingcart/MainActivity.kt
+++ app/src/main/java/com/example/shoopingcart/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ShoopingCartTheme {
-                // A surface container using the 'background' color from the theme
+             
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
